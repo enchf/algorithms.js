c02d26cca7419d1989cfbdc8e82fd885d70acc60
enchf <e.ernesto.espinosa@gmail.com>
2016-05-25 18:13:14 +0100

    Added message to Roman testing

--- test/suites/numbers.test.js
+++ test/suites/numbers.test.js
@@ -2,14 +2,17 @@ Numbers.test = {};
 
 Numbers.test.toRoman = function(assert, json) {
   var test;
+  var message = function(param, expected) {
+	return 'Converting ' + param + ' into ' + expected;
+  };
     
   for (var i = 1; i <= json.numbers.length; i++) {
-    assert.strictEqual(Numbers.toRoman(i), json.numbers[i-1]);
+    assert.strictEqual(Numbers.toRoman(i), json.numbers[i-1], message(i, json.numbers[i-1]));
   }
     
   for (var i = 0; i < json.special.length; i++) {
     test = json.special[i];
-    assert.strictEqual(Numbers.toRoman(test.params), test.expected);
+    assert.strictEqual(Numbers.toRoman(test.params), test.expected, message(test.params, test.expected));
   }
 };
 
